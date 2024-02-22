0e760a68058587b683b9d8f0cd2a3e832a9a56fe
Luan. Vo <dynamic2500@gmail.com>
2024-02-22 15:49:15 +0700

    support variable [realstream] split from stream name <stream@desc>

--- trunk/src/app/srs_app_utility.cpp
+++ trunk/src/app/srs_app_utility.cpp
@@ -80,6 +80,8 @@ string srs_path_build_stream(string template_path, string vhost, string app, str
     path = srs_string_replace(path, "[app]", app);
     // variable [stream]
     path = srs_string_replace(path, "[stream]", stream);
+    // variable [realstream]
+    path = srs_string_replace(path, "[realstream]", stream.substr(0,stream.find("@")));
     
     return path;
 }
